d704bb05264068b9c97921c464b44e65df16a3bf
Ryan Tse <rytse@umd.edu>
2020-02-01 16:31:20 -0500

    Try different background opacity

--- lib/widgets/about_me.dart
+++ lib/widgets/about_me.dart
@@ -21,13 +21,14 @@ class AboutMe extends StatelessWidget {
       Container(
         height: 750.0,
         decoration: BoxDecoration(
-            color: Colors.white,
+            //color: Colors.white,
+            color: Colors.black,
             gradient: LinearGradient(
                 begin: FractionalOffset.topCenter,
                 end: FractionalOffset.bottomCenter,
                 colors: [
-                  Colors.grey.withOpacity(0.3),
-                  Colors.black,
+                  Colors.black.withOpacity(0.2),
+                  Colors.black.withOpacity(0.5),
                 ],
                 stops: [
                   0.0,
@@ -73,7 +74,7 @@ class AboutMe extends StatelessWidget {
                       // Add your onPressed code here!
                     },
                     label: Text(
-                      'Download my resume',
+                      'Download my résumé',
                       style: TextStyle(fontSize: 20),
                     ),
                     icon: Icon(Icons.file_download),
@@ -95,34 +96,41 @@ class AboutMe extends StatelessWidget {
                   alignment: Alignment.center,
                   //color: Colors.indigo[400].withOpacity(0.85),
                   //color: Colors.tealAccent[400].withOpacity(0.55),
-                  color: Color(0x062726).withOpacity(0.75),
-                  child: Column(children: <Widget>[
-                    //Text('About Me:',
-                    Text('ABOUT ME:',
-                        style: GoogleFonts.montserrat(
-                          //style: GoogleFonts.libreFranklin(
-                          textStyle: TextStyle(
-                              fontSize: 42,
-                              //color: Color(0xecd9f4).withOpacity(0.90)),
-                              color: Colors.white.withOpacity(0.90)),
-                        )),
+                  //color: Color(0x062726).withOpacity(0.85),
+                  color: Color(0x020f0e).withOpacity(0.75),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        //Text('About Me:',
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 0),
+                          child: Text('ABOUT ME',
+                              style: GoogleFonts.montserrat(
+                                //style: GoogleFonts.libreFranklin(
+                                textStyle: TextStyle(
+                                    fontSize: 42,
+                                    //color: Color(0xecd9f4).withOpacity(0.90)),
+                                    color: Colors.white.withOpacity(0.90)),
+                              )),
+                        ),
 
-                    Container(
-                        //color: Colors.indigo[300].withOpacity(0.8),
-                        //   color: Colors.tealAccent[400].withOpacity(0.0),
-                        color: Color(0x102b3f).withOpacity(0),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 20),
-                        child: Text(
-                          'I’m a freshman at the University of Maryland, College Park studying Electrical Engineering and Applied Math. I like working on control systems and modeling things. I’m also an Eagle Scout from Troop 1449. Check out my GitHub and my résumé.',
-                          style: GoogleFonts.openSans(
-                            textStyle: TextStyle(
-                                fontSize: 25,
-                                //  color: Color(0xecd9f4).withOpacity(0.75)),
-                                color: Colors.white.withOpacity(0.90)),
-                          ),
-                        )),
-                  ]),
+                        Container(
+                            //color: Colors.indigo[300].withOpacity(0.8),
+                            //   color: Colors.tealAccent[400].withOpacity(0.0),
+                            color: Color(0x102b3f).withOpacity(0),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Text(
+                              'I’m a freshman at the University of Maryland, College Park studying Electrical Engineering and Applied Math. I like working on control systems and modeling things. I’m also an Eagle Scout from Troop 1449. Check out my GitHub and my résumé.',
+                              style: GoogleFonts.openSans(
+                                textStyle: TextStyle(
+                                    fontSize: 25,
+                                    //  color: Color(0xecd9f4).withOpacity(0.75)),
+                                    color: Colors.white.withOpacity(0.90)),
+                              ),
+                            )),
+                      ]),
                 ),
               ),
             )
